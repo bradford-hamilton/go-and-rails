2558dacb9dfbc790a37a88d7d323acae1ad31eaf
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-10-22 15:05:05 -0600

    another

--- config/initializers/quartz.rb
+++ config/initializers/quartz.rb
@@ -1,12 +1,12 @@
-module Quartz::Validations
-  def self.check_for_go
-    puts ENV['PATH']
-    go_exists = ENV['PATH'].split(File::PATH_SEPARATOR).any? do |directory|
-      File.exist?(File.join(directory, 'go'))
-    end
+# module Quartz::Validations
+#   def self.check_for_go
+#     puts ENV['PATH']
+#     go_exists = ENV['PATH'].split(File::PATH_SEPARATOR).any? do |directory|
+#       File.exist?(File.join(directory, 'go'))
+#     end
 
-    raise Quartz::ConfigError, 'Go not installed.' unless go_exists
-  end
-end
-
+#     raise Quartz::ConfigError, 'Go not installed.' unless go_exists
+#   end
+# end
+puts ENV['PATH']
 Quartz = Quartz::Client.new(file_path: 'app/go/exported_functions.go')
