bc96c11d7c7d073e50d4549da086ea0b53887220
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-10-22 16:24:09 -0600

    new-master

--- config/initializers/quartz.rb
+++ config/initializers/quartz.rb
@@ -1,12 +1,21 @@
-# module Quartz::Validations
-#   def self.check_for_go
-#     puts ENV['PATH']
-#     go_exists = ENV['PATH'].split(File::PATH_SEPARATOR).any? do |directory|
-#       File.exist?(File.join(directory, 'go'))
-#     end
+# system("sudo wget -c https://storage.googleapis.com/golang/go1.12.linux-amd64.tar.gz")
+# system("sudo tar -C /usr/local -xvzf go1.12.linux-amd64.tar.gz")
+# system("export PATH=$PATH:$GOPATH/bin")
+# system("go get github.com/DavidHuie/quartz/go/quartz")
 
-#     raise Quartz::ConfigError, 'Go not installed.' unless go_exists
-#   end
-# end
-puts ENV['PATH']
+# system("sudo apt-get install golang-go")
+# system("export PATH=$PATH:/usr/local/go/bin")
+# system("go get github.com/DavidHuie/quartz/go/quartz")
+
+module Quartz::Validations
+  def self.check_for_go
+    puts ENV['PATH']
+    go_exists = ENV['PATH'].split(File::PATH_SEPARATOR).any? do |directory|
+      File.exist?(File.join(directory, 'go'))
+    end
+
+    raise Quartz::ConfigError, 'Go not installed.' unless go_exists
+  end
+end
+
 Quartz = Quartz::Client.new(file_path: 'app/go/exported_functions.go')
